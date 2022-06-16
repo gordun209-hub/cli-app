683d084d30d4902d79ee445c5063dd081fd80226
gordun209 <ali-han80@hotmail.com>
2022-06-16 19:12:28 +0300

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        // --snip--
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -21,10 +20,41 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // --snip--
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // create new empty vector
+    let mut results = Vec::new();
+// for content in lines push line to vector(result)
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,8 @@ use cli_app::Config;
 use std::env;
 use std::process;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
-        assert_eq!(vec["safe, fast, productive."], search(query, contents));
-    }
-}
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
-}
+
+
 fn main() {
     // --snip--
     let args: Vec<String> = env::args().collect();
